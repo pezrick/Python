01f7f9a4b7573d1984ce1ed3c3b4635147e6898c
pezrick <pezrick@gmail.com>
2021-11-02 17:15:55 +0000

    function grades

--- overall_grade.py
+++ overall_grade.py
@@ -1,5 +1,12 @@
 def student_grade(name, homework_score, assessment_score, final_exam_score):
-    total_percentage = round((homework_score + assessment_score + final_exam_score) / 175 * 100)
-    return name, total_percentage
+    if homework_score > 25:
+        return "Invalid Amount Homework"
+    elif assessment_score > 50:
+        return "Invalid Amount Assessment"
+    elif final_exam_score > 100:
+        return "Invalid Amount Final Exam"
+    else:
+        total_percentage = round((homework_score + assessment_score + final_exam_score) / 175 * 100)
+        return name, total_percentage
 
-print(student_grade("Pez", 20, 50, 90))
+print(student_grade("Pez", 23, 39, 80))
