d33451c50cb1dc4e5bdf7f0de5e11073a3437ff0
pezrick <pezrick@gmail.com>
2021-11-02 14:05:16 +0000

    gitlab first go

---
+++ grade_calculator.py
@@ -0,0 +1,17 @@
+maths_grade = int(input("What is your maths grade: "))
+chemistry_grade = int(input("What is your chemistry grade: "))
+physics_grade = int(input("What is your physics grade: "))
+average_grade = int((maths_grade + chemistry_grade + physics_grade) / 3)
+
+if average_grade >= 70:
+    print("A")
+elif average_grade >= 60:
+    print("B")
+elif average_grade >= 50:
+    print("C")
+elif average_grade >= 40:
+    print("D")
+else:
+    print("You are below 20 years old")
+
+print(average_grade)
---
+++ near_challenge.py
@@ -0,0 +1,12 @@
+first_string = str(input("What is your first string: "))
+second_string = str(input("What is your second string: "))
+
+x = len(first_string)
+print (x)
+
+
+
+print(first_string)
+
+
+# print(first_string, " ", second_string)
